3dba4c58359c4b438d46f0ec05305212187681d2
gunjou <gjoefandi@gmail.com>
2025-04-10 14:31:08 +0800

    update status gagal

--- api/absensi.py
+++ api/absensi.py
@@ -59,7 +59,7 @@ def absensi():
         'lokasi_masuk': row['lokasi_masuk'],
         'lokasi_keluar': row['lokasi_keluar'],
         'waktu_terlambat': hitung_keterlambatan(row['jam_masuk']),
-        'selisih': hitung_selisih_waktu(row['jam_masuk'], row['jam_keluar'])
+        'jumlah_jam_kerja': hitung_selisih_waktu(row['jam_masuk'], row['jam_keluar'])
     } for index, row in enumerate(list_absensi)]
     
     return {'absensi': result}, 200  # Mengembalikan hasil dalam format JSON
@@ -94,93 +94,104 @@ def check_wajah(id_karyawan):
 def check_in(id_karyawan):
     # Periksa apakah file gambar ada dalam request
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
+        return jsonify({'error': 'Tidak ada foto yang anda masukkan!'}), 400
     
     # Periksa apakah koordinat lokasi ada dalam request
     user_lat = request.form.get('latitude')  # Ambil dari form-data
     user_lon = request.form.get('longitude')  # Ambil dari form-data
 
     if not user_lat or not user_lon:
-        return jsonify({'error': 'Location data is required'}), 400
+        return jsonify({'error': 'Anda belum memberikan lokasi!'}), 400
 
     # Konversi latitude dan longitude ke float
     try:
         user_lat = float(user_lat)
         user_lon = float(user_lon)
     except ValueError:
-        return jsonify({'error': 'Invalid latitude or longitude'}), 400
+        return jsonify({'error': f'Format data lokasi anda salah. Latitude: {user_lat}, Longitude: {user_lon}'}), 400
 
     # Cek apakah user dalam radius lokasi yang diizinkan
     lokasi_absensi = get_valid_office_name(user_lat, user_lon)
 
     if lokasi_absensi is None:
-        return jsonify({'status': 'error', 'message': 'You are outside the attendance area'}), 403
+        return jsonify({'status': 'error', 'message': 'Anda berada diluar lokasi kerja, lakukan absensi di lokasi yang sudah ditentukan!'}), 403
 
     # Upload file
     image = request.files['file']
 
     # Periksa apakah file memiliki nama
     if image.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify({'error': 'Anda belum memasukkan gambar!'}), 400
     
     # Verifikasi wajah
     face = verifikasi_wajah(id_karyawan, image)
-    if face:
-        tanggal, jam_masuk = get_timezone()
-        
-        # Menambahkan check-in
-        result = add_checkin(id_karyawan, tanggal, jam_masuk, lokasi_absensi)
-        if result is None:
-            return jsonify({'status': 'Failed to record check-in'}), 500
-        
-        return jsonify({'status': 'Check-in succeeded', 'lokasi': lokasi_absensi}), 200
-    else:
-        return jsonify({'status': 'Face not match'}), 403
+    
+    if face == "not_detected":
+        return jsonify({'status': 'error', 'message': 'Wajah tidak terdeteksi dalam gambar. Pastikan wajah terlihat jelas!'}), 400
+    elif face == "error":
+        return jsonify({'status': 'error', 'message': 'Terjadi kesalahan saat memverifikasi wajah.'}), 500
+    elif not face:
+        return jsonify({'status': 'error', 'message': 'Wajah anda tidak sesuai dengan akun!'}), 403
+
+    tanggal, jam_masuk = get_timezone()
+    
+    # Menambahkan check-in
+    result = add_checkin(id_karyawan, tanggal, jam_masuk, lokasi_absensi)
+    if result is None:
+        return jsonify({'status': 'error', 'message': 'Data yang anda masukkan belum lengkap!'}), 500
+    
+    return jsonify({'status': 'Check-in succeeded', 'lokasi': lokasi_absensi}), 200
+
     
 @absensi_bp.route('/absensi/<int:id_karyawan>', methods=['PUT'])
 @jwt_required()
 def check_out(id_karyawan):
     # Periksa apakah file gambar ada dalam request
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
+        return jsonify({'error': 'Tidak ada foto yang anda masukkan!'}), 400
 
     # Ambil latitude dan longitude dari form-data
     user_lat = request.form.get('latitude')
     user_lon = request.form.get('longitude')
 
     if not user_lat or not user_lon:
-        return jsonify({'error': 'Location data is required'}), 400
+        return jsonify({'error': 'Anda belum memberikan lokasi!'}), 400
 
     # Konversi koordinat ke float
     try:
         user_lat = float(user_lat)
         user_lon = float(user_lon)
     except ValueError:
-        return jsonify({'error': 'Invalid latitude or longitude'}), 400
+        return jsonify({'error': f'Format data lokasi anda salah. Latitude: {user_lat}, Longitude: {user_lon}'}), 400
 
     # Cek apakah user dalam radius lokasi yang diizinkan
     lokasi_absensi = get_valid_office_name(user_lat, user_lon)
 
     if lokasi_absensi is None:
-        return jsonify({'status': 'error', 'message': 'You are outside the attendance area'}), 403
+        return jsonify({'status': 'error', 'message': 'Anda berada diluar lokasi kerja, lakukan absensi di lokasi yang sudah ditentukan!'}), 403
 
     # Upload file
     image = request.files['file']
 
     # Periksa apakah file memiliki nama
     if image.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify({'error': 'Anda belum memasukkan gambar!'}), 400
     
     # Verifikasi wajah
     face = verifikasi_wajah(id_karyawan, image)
-    if face:
-        tanggal, jam_keluar = get_timezone()
-        
-        # Update jam keluar
-        result = update_checkout(id_karyawan, tanggal, jam_keluar, lokasi_absensi)
-        if result is None or result == 0:
-            return jsonify({'status': 'Failed to record check-out or no check-in found'}), 500
-        
-        return jsonify({'status': 'Check-out succeeded', 'lokasi': lokasi_absensi}), 200
-    else:
-        return jsonify({'status': 'Face not match'}), 403
+
+    if face == "not_detected":
+        return jsonify({'status': 'error', 'message': 'Wajah tidak terdeteksi dalam gambar. Pastikan wajah terlihat jelas!'}), 400
+    elif face == "error":
+        return jsonify({'status': 'error', 'message': 'Terjadi kesalahan saat memverifikasi wajah.'}), 500
+    elif not face:
+        return jsonify({'status': 'error', 'message': 'Wajah anda tidak sesuai dengan akun!'}), 403
+
+    tanggal, jam_keluar = get_timezone()
+    
+    # Update jam keluar
+    result = update_checkout(id_karyawan, tanggal, jam_keluar, lokasi_absensi)
+    if result is None or result == 0:
+        return jsonify({'status': 'error', 'message': 'Gagal mencatat absen pulang atau tidak ditemukan absen masuk sebelumnya'}), 500
+    
+    return jsonify({'status': 'Absen pulang berhasil', 'lokasi': lokasi_absensi}), 200
--- api/face_detection.py
+++ api/face_detection.py
@@ -2,19 +2,25 @@ import face_recognition
 
 
 def verifikasi_wajah(id_karyawan, image):
-    # get file and filename
-    file_path = f"./cropped_faces/{id_karyawan}.jpg"
-    # file_path = f"./foto_pegawai/{id_karyawan}.jpg"
-    # file_name = os.path.splitext(os.path.basename(file_path))[0]
+    try:
+        file_path = f"./cropped_faces/{id_karyawan}.jpg"
 
-    # Load image
-    known_image = face_recognition.load_image_file(file_path)
-    unknown_image = face_recognition.load_image_file(image)
-    # Image embedding
-    known_encoding = face_recognition.face_encodings(known_image)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        known_image = face_recognition.load_image_file(file_path)
+        unknown_image = face_recognition.load_image_file(image)
 
-    # Compare image
-    results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        known_encodings = face_recognition.face_encodings(known_image)
+        unknown_encodings = face_recognition.face_encodings(unknown_image)
 
-    return results[0]
+        if len(known_encodings) == 0 or len(unknown_encodings) == 0:
+            return "not_detected"
+
+        known_encoding = known_encodings[0]
+        unknown_encoding = unknown_encodings[0]
+
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+
+        return results[0]
+    
+    except Exception as e:
+        print(f"[ERROR verifikasi_wajah] {e}")
+        return "error"
